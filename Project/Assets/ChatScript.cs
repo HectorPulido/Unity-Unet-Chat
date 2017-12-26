bc77d665223058c4212cdec3c946c7e76a837b18
Hector Pulido <Hectorandrespp@gmail.com>
2017-12-25 19:19:54 -0500

    First Commit

---
+++ Project/Assets/ChatScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ChatScript : NetworkBehaviour
+{
+	Text TxtTexto;
+	InputField inputField;
+
+	void Start () 
+	{
+		TxtTexto = GameObject.Find ("TxtTexto").GetComponent < Text>();
+		inputField = GameObject.Find ("input").GetComponent<InputField> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!isLocalPlayer)
+			return;
+
+
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			if(inputField.text != "")
+			{
+				string Mensaje = inputField.text;
+				inputField.text = "";
+
+				CmdEnviar (Mensaje);
+			}
+		}
+	}
+
+	[Command]
+	void CmdEnviar(string mensaje)
+	{
+		RpcRecivir (mensaje);
+
+	}
+
+	[ClientRpc]
+	public void RpcRecivir(string mensaje)
+	{
+		TxtTexto.text += ">>" + mensaje + "\n";
+	}
+
+
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Unity Unet Simple Chat
+
+A simple online chat made with Unity and Unet
+The project is coded in C#
+
+This tutorial was made for Hector Pulido for his youtube channel 
+https://www.youtube.com/channel/UCS_iMeH0P0nsIDPvBaJckOw
+
+Please consider Support on Patreon
+https://www.patreon.com/HectorPulido
+
